91963d8be972aa89c29afc0b164f8dbfc7aa4b93
Prateek Ganguli <prateek.ganguli@gmail.com>
2024-07-08 17:13:35 +0530

    CODE FOR MAIN.PY AND MAIN_LUBY.PY UPDATED.

--- main_luby.py
+++ main_luby.py
@@ -1,5 +1,4 @@
 #!/home/prateek/deepgate2-venv/bin/python3
-import shutil
 import argparse
 import subprocess
 import os
@@ -23,6 +22,7 @@ bmc_data_last_depth: str = ""
 def run_engine(selected_engine: str, circuit: str, FLAGS: str) -> list[str]:
     global bmc_data_last_depth
     output = []
+    max_depth = None
 
     if selected_engine == "bmc3j":
         command = [
@@ -39,11 +39,6 @@ def run_engine(selected_engine: str, circuit: str, FLAGS: str) -> list[str]:
             "abc",
             f"-c read {circuit}; print_stats; &get; bmc3 -g {FLAGS}; print_stats",
         ]
-    elif selected_engine == "bmc3r":
-        command = [
-            "abc",
-            f"-c read {circuit}; print_stats; &get; bmc3 -r {FLAGS}; print_stats",
-        ]
     elif selected_engine == "bmc3s":
         command = [
             "abc",
@@ -53,11 +48,6 @@ def run_engine(selected_engine: str, circuit: str, FLAGS: str) -> list[str]:
         command = [
             "abc",
             f"-c read {circuit}; print_stats; &get; bmc3 -u {FLAGS}; print_stats",
-        ]
-    elif selected_engine == "bmc2":
-        command = [
-            "abc",
-            f"-c read {circuit}; print_stats; &get; bmc2 {FLAGS}; print_stats",
         ]
     else:
         raise ValueError(f"Unknown engine: {selected_engine}")
@@ -76,104 +66,70 @@ def run_engine(selected_engine: str, circuit: str, FLAGS: str) -> list[str]:
         elif re.match(r"^\d+ \+ :", line):
             bmc_data_last_depth = line
             output.append(line)
-    return bmc_data_last_depth
+            max_depth = line
+    return max_depth
 
-
-
-def process_circuits(end_time: float, args: any, FLAGS: str, CURRENT_FRAME: int):
+def process_circuits(end_time: float, args: any, FLAGS: str, CURRENT_FRAME: int, modified_time: int):
     time_stamp = time.time()
     engine = None
     input_circuit_name = os.path.basename(args.input_circuit).split(".")[0]
 
-    """# Unfold known circuits
-    for file in os.listdir(args.known_circuit_path):
-        if file.endswith(".aig") and input_circuit_name not in file:
-            unfold_circuit(f"{args.known_circuit_path}/{file}", args.UNFOLD_FRAME, args.unfold_path)"""
     # Unfold input circuit
+    print(f'Unfolding at depth: {args.UNFOLD_FRAME}\n')
     input_circuit_unfolded = unfold_circuit(
         args.input_circuit, args.UNFOLD_FRAME, args.unfold_path
     )
     # Find most similar circuit and extract frame times
     best_friend = most_similar_circuit(
         input_circuit_unfolded, args.UNFOLD_FRAME, args.unfold_path
-    )
-    
+    ) 
     engine_list = extract_frame_time(args.UNFOLD_FRAME, args.csv_path, best_friend)
     # Select engine with minimum time
     min_time = min(engine[2][0] for engine in engine_list)
     selected_engines = [engine for engine in engine_list if engine[2][0] == min_time]
-    print(selected_engines)
+    formatted_selected_engines =  '     '.join(f"({i[0]} | {i[1]} | {i[2][0]})" for i in selected_engines)
+    print(f"{formatted_selected_engines}\n")
     for e in selected_engines:
         if 'bmc3g' == e[0]:
             engine = e[0]
             break
         else:
             engine = random.choice(selected_engines)[0]
-
     if engine is None:
-        args.T = int(end_time - time.time())
-        FLAGS = f"-S {args.UNFOLD_FRAME} -T {args.T} -F 0 -v"
+        modified_time = int(end_time - time.time())
+        FLAGS = f"-S {args.UNFOLD_FRAME} -T {modified_time} -F 0 -v"
         print(
-            f"No engine found, hence running bmc3 -g for the remaining time: {args.T} secs\n"
+            f"No engine found, hence running bmc3g for the remaining time: {modified_time} secs\n"
         )
 
-        depth_reached = run_engine("bmc3 -g", args.input_circuit, FLAGS 
-                )
-        bmc_data_last_depth = depth_reached  ###
-
-        if depth_reached is None:  ###
-            return args.UNFOLD_FRAME, -1, False, None, depth_reached
-
-        if "+" in depth_reached: ###
-            args.UNFOLD_FRAME = extract_data(depth_reached[-1])[0]
-        else:
-            args.UNFOLD_FRAME = int(
-                [
-                    part.split("=")[1].strip()
-                    for part in depth_reached[-1].split(".")
-                    if "F =" in part
-                ][0]
-            )
-
-        print(f"FINAL FRAME REACHED IS {bmc_data_last_depth}\n")
-        return args.UNFOLD_FRAME, -1, False, "bmc3g", depth_reached
-
+        depth_reached = run_engine("bmc3g", args.input_circuit, FLAGS 
+        )
     print(
         f"DEPTH ({args.UNFOLD_FRAME}): DeepGate2 execution + BMC engine selection: {round(time.time() - time_stamp, 2)} secs\n"
-    )
+        )
     print(
         f"Outcome at DEPTH ({args.UNFOLD_FRAME}): Most similar circuit: {best_friend}.aig, Best BMC engine for {os.path.basename(args.input_circuit)} at Depth {args.UNFOLD_FRAME}: {engine}\n"
     )
-    depth_reached = run_engine(engine, args.input_circuit, FLAGS)
-    bmc_data_last_depth = depth_reached ###
-
-    if depth_reached is None: ###
+    depth_reached = run_engine(engine, args.input_circuit, FLAGS) ###
+    print(f'{depth_reached}\n') ###
+    if depth_reached is None:
+        modified_time = int(end_time - time.time())
+        FLAGS = f"-S {args.UNFOLD_FRAME} -T {modified_time} -F 0 -v"
         print(
-            f"Running {engine} on {os.path.basename(args.input_circuit)} for {args.T} seconds, Depth reached: {args.UNFOLD_FRAME}\n"
-        )
-        print(f"{bmc_data_last_depth}\n")
-        return args.UNFOLD_FRAME, -1, True, engine, depth_reached
+             f"Since depth_reached is None, running bmc3g for the remaining time: {modified_time} secs\n"
+         )
+        depth_reached = run_engine("bmc3g", args.input_circuit, FLAGS)
 
-    if "+" in depth_reached: ###
-        args.UNFOLD_FRAME = extract_data(depth_reached)[0] ###
-    else:
-        args.UNFOLD_FRAME = int(
-            [
-                part.split("=")[1].strip()
-                for part in depth_reached.split(".") ###
-                if "F =" in part
-            ][0]
-        )
+    if "+" in depth_reached:
+        args.UNFOLD_FRAME = extract_data(depth_reached)[0]
     print(
-        f"Running {engine} on {os.path.basename(args.input_circuit)} for {args.T} seconds, Depth reached: {args.UNFOLD_FRAME}\n"
+        f"Running {engine} on {os.path.basename(args.input_circuit)} for {modified_time} seconds, Depth reached: {args.UNFOLD_FRAME}\n"
     )
-    print(f"{bmc_data_last_depth}\n")
 
     if CURRENT_FRAME == args.UNFOLD_FRAME:
         return args.UNFOLD_FRAME, -1, True, engine, depth_reached
 
     return args.UNFOLD_FRAME, args.UNFOLD_FRAME, True, engine, depth_reached
-
 
 def extract_data(data: str):
     predictors = re.sub(r"[^0-9 \.]", "", data)
@@ -185,21 +141,18 @@ def extract_data(data: str):
     predictors = [float(num) if "." in num else int(num) for num in predictors]
     return predictors
 
-
 def terminate_process(signum: int, frame: any) -> None:
     global bmc_data_last_depth
     print("=" * 80 + " TIMEOUT " + "=" * 80)
     input = os.path.basename(args.input_circuit).split('.')[0]
-    shutil.rmtree(f"{args.unfold_path}/{input}")
     print(f"\n{bmc_data_last_depth}\n")
     os._exit(1)
-
 
 def fixed_time_partition_mode(args: any) -> None:
     global bmc_data_last_depth
     start_time = time.time()
+    modified_time = args.T
     end_time = start_time + args.total_time
-    # modified_time = args.T
     first_iteration = True
     START_FRAME = 0
     CURRENT_FRAME = 0
@@ -226,8 +179,9 @@ def fixed_time_partition_mode(args: any) -> None:
                 FLAGS = f"-S {START_FRAME} -T {args.T} -F 0 -v"
                 if first_similarity_check_done is not True:
                     args.UNFOLD_FRAME, START_FRAME, continue_loop, engine, depth_reached = (
-                    process_circuits(end_time, args, FLAGS, CURRENT_FRAME)
-                    )
+                    process_circuits(end_time, args, FLAGS, CURRENT_FRAME, modified_time))
+                    args.UNFOLD_FRAME += 1
+                    START_FRAME += 1
                     if not continue_loop:
                         break
                     first_similarity_check_done = True
@@ -236,88 +190,76 @@ def fixed_time_partition_mode(args: any) -> None:
 
                 if first_similarity_check_done:
                     last_depth_data = extract_data(depth_reached) ###
-                    time_wasted = round(args.T - float(last_depth_data[7]), 2)
+                    time_wasted = round(modified_time - float(last_depth_data[7]), 2)
                     print(f"Time wasted in previous iteration: {time_wasted} sec")
-                    if time_wasted <= args.p*args.T:
+                    if time_wasted <= args.p*modified_time:
                         print(f'\nTime wasted {time_wasted} < {args.T*args.p}')
                         luby_i += 1
                         print(f"\nNext time slot = {args.T} * luby({luby_i}) = {args.T} * {luby(luby_i)} = {args.T * luby(luby_i)}")
-                        args.T *= luby(luby_i)
-                        FLAGS = f"-S {START_FRAME} -T {args.T} -F 0 -v"
+                        modified_time = args.T * luby(luby_i)
+                        FLAGS = f"-S {START_FRAME} -T {modified_time} -F 0 -v"
                         print(f"\nStarting at DEPTH ({START_FRAME}): \n")
 
                         depth_reached = run_engine(engine, args.input_circuit, FLAGS)
-                        bmc_data_last_depth = depth_reached ###
+                        print(f'{depth_reached}\n') ###
 
                         if depth_reached is None: ###
+                            modified_time = int(end_time - time.time()-5)
+                            FLAGS = f"-S {START_FRAME} -T {modified_time} -F 0 -v"
                             print(
-                                f"Running {engine} on {os.path.basename(args.input_circuit)} for {args.T} seconds, Depth reached: {args.UNFOLD_FRAME}\n"
-                            )   
-                            print(f"{bmc_data_last_depth}\n")
-                            START_FRAME = -1
-                            first_similarity_check_done = True
-                            continue
-                        if "+" in depth_reached: ###
-                            args.UNFOLD_FRAME = int(
-                            depth_reached ###
-                                .split(":")[0]
-                                .strip()
-                                .split("+")[0]
-                                .strip()
+                            f"Since depth_reached is None, running bmc3g for the remaining time: {modified_time} secs\n"
                             )
-                        else:
-                             args.UNFOLD_FRAME = int(
-                                [
-                                    part.split("=")[1].strip()
-                                    for part in depth_reached.split(".") ###
-                                    if "F =" in part
-                                ][0]
-                            )  
+                            depth_reached = run_engine("bmc3g", args.input_circuit, FLAGS)
+                            print(f"{bmc_data_last_depth}\n")
+                            break
+                            print(f"Running {engine} on {args.input_circuit.split('/')[-1]} for {modified_time} seconds, Depth reached: {args.UNFOLD_FRAME}\n" )
+                        if "+" in depth_reached: ###
+                            args.UNFOLD_FRAME = extract_data(depth_reached)[0]
                         START_FRAME = args.UNFOLD_FRAME
                         if CURRENT_FRAME == START_FRAME:
                             START_FRAME = -1
-
-                        print(
-                             f"Running {engine} on {args.input_circuit.split('/')[-1]} for {args.T} seconds, Depth reached: {args.UNFOLD_FRAME}\n"
-                        )
-                        print(f"{bmc_data_last_depth}\n")
+                        else:
+                            args.UNFOLD_FRAME +=1
+                            START_FRAME +=1
+                        
                         first_similarity_check_done = True
                         continue
     
                     else:
                         print("Time wastage exceeded permissible limit, computing new similar circuit")
-                        args.T *= 2
+                        luby_i += 1
+                        print(f"\nNext time slot = {args.T} * luby({luby_i}) = {args.T} * {luby(luby_i)} = {args.T * luby(luby_i)}")
+                        modified_time = args.T  *  luby(luby_i)
+                        FLAGS = f"-S {START_FRAME} -T {modified_time} -F 0 -v"
                         args.UNFOLD_FRAME, START_FRAME, continue_loop, engine, depth_reached = (
-                        process_circuits(end_time, args, FLAGS, CURRENT_FRAME)
+                        process_circuits(end_time, args, FLAGS, CURRENT_FRAME, modified_time)
                         )
+                        args.UNFOLD_FRAME += 1
+                        START_FRAME += 1
                         if not continue_loop:
                             break
                         continue
             else:
                 print("No progress, computing new similar circuit")
-                args.T *= 2
+                luby_i += 1
+                print(f"\nNext time slot = {modified_time} * luby({luby_i}) = {args.T} * {luby(luby_i)} = {args.T * luby(luby_i)}")
+                modified_time = args.T * luby(luby_i)
+                FLAGS = f"-S {START_FRAME} -T {modified_time} -F 0 -v"
                 args.UNFOLD_FRAME, START_FRAME, continue_loop, engine, depth_reached = (
-                process_circuits(end_time, args, FLAGS, CURRENT_FRAME)
+                process_circuits(end_time, args, FLAGS, CURRENT_FRAME, modified_time)
                 )
+                args.UNFOLD_FRAME += 1
+                START_FRAME += 1
                 if not continue_loop:
                     break
                 continue
-
-
-            '''args.UNFOLD_FRAME, START_FRAME, continue_loop, engine, depth_reached = (
-                process_circuits(end_time, args, FLAGS, CURRENT_FRAME)
-            )
-            if not continue_loop:
-                break
-            first_similarity_check_done = True'''
 
     except Exception as e:
         print(f"An exception occurred: {e}")
     finally:
         signal.alarm(0)
 
-
-def variable_time_partition_mode(args: any) -> None:
+'''def variable_time_partition_mode(args: any) -> None:
     with open("data/model.pkl", "rb") as pkl_file:
         model_dict = pickle.load(pkl_file)
 
@@ -428,12 +370,10 @@ def variable_time_partition_mode(args: any) -> None:
     except Exception as e:
         print(f"An exception occurred: {e}")
     finally:
-        signal.alarm(0)
-
+        signal.alarm(0)'''
 
 def valid_range(value):
     return float(value) if 0 <= float(value) <= 1 else argparse.ArgumentTypeError(f"Value must be between 0 and 1. Provided value: {value}")
-
 
 def main():
     global args
@@ -441,7 +381,7 @@ def main():
         description="BMC Sequence Script", add_help=False
     )
     group = initial_parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-v", action="store_true", help="Run in mode --> Variable")
+    #group.add_argument("-v", action="store_true", help="Run in mode --> Variable")
     group.add_argument("-f", action="store_true", help="Run in mode --> Fixed")
     initial_args, remaining_argv = initial_parser.parse_known_args()
 
@@ -476,10 +416,10 @@ def main():
 
     if initial_args.f:
         fixed_time_partition_mode(args)
-    elif initial_args.v:
-        variable_time_partition_mode(args)
+    #elif initial_args.v:
+        #variable_time_partition_mode(args)
     else:
-        print("Please specify a mode with -f or -v")
+        print("Please specify a mode with -f")
 
 
 if __name__ == "__main__":
